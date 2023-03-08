22a6ae8fcf69d7024e171de07c451a2a4c95545c
Matthew Smart <matthewsmart13@gmail.com>
2023-03-08 10:09:53 -0600

    Added App class Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-	return <div></div>;
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			task: { text: "" },
+			tasks: [],
+		};
+	}
+
+	render() {
+		const { task, tasks } = this.state;
+
+		return (
+			<div>
+				<form>
+					<label htmlFor="taskInput">Enter task</label>
+					<input type="text" id="taskInput" />
+					<button type="submit">Add Task</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
