0b8d5c656c45b29d3353c4bb989a91093eca0caa
Matthew Smart <matthewsmart13@gmail.com>
2023-04-24 16:19:28 -0500

    Started fleshing out the component

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,12 +1,27 @@
 import { useState } from "react";
 
-const TaskForm = () => {
+const TaskForm = (props) => {
+	const [buffer, setBuffer] = useState({ ...props.task });
+
+	const handleChange = (key, value) => {
+		setBuffer((buffer) => {
+			return { ...buffer, [key]: value };
+		});
+	};
+
 	return (
 		<div>
 			<form>
-				<label></label>
-				<input></input>
+				<label htmlFor="taskInput">Task</label>
+				<input
+					onChange={(e) => {
+						handleChange("text", e.target.value);
+					}}
+					type="text"
+					value={buffer.text}></input>
 			</form>
 		</div>
 	);
 };
+
+export default TaskForm;
