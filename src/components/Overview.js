57d20a7d2de5d2a596049c8d0c418a686934fd15
Matthew Smart <matthewsmart13@gmail.com>
2023-05-01 20:40:37 -0500

    Removed: List render from the Component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,22 +1,15 @@
-import { useState, useEffect } from "react";
-import TaskForm from "./TaskForm";
-
 const Overview = (props) => {
-	const [editing, setEdit] = useState(true);
 	return (
-		<ul>
-			{props.taskList.map((task) => {
-				return (
-					<li key={task.id}>
-						<div>
-							{task.taskNumber} {task.text}
-							<button>Delete</button>
-							<button>Edit</button>
-						</div>
-					</li>
-				);
-			})}
-		</ul>
+		<div>
+			{props.task.taskNumber} {props.task.text}
+			<button>Delete</button>
+			<button
+				onClick={() => {
+					prop.handleEditButtonClick();
+				}}>
+				Edit
+			</button>
+		</div>
 	);
 };
 
